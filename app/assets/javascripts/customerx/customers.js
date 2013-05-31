32c83efc4106eb53a8ca1109496cfb978947d799
jc <emclab2011@gmail.com>
2013-05-31 11:56:53 -0500

    JC: added datepicker format yy-mm-dd in customer.js for customer_since.

--- app/assets/javascripts/customerx/customers.js
+++ app/assets/javascripts/customerx/customers.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function() {
-   $("#customer_since_date").datepicker()  //({dateFormat: 'yy-mm-dd'});
+   $("#customer_since_date").datepicker({dateFormat: 'yy-mm-dd'});
 });
 
 //for search.html.erb
